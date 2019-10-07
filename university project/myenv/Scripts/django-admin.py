0448ebce6607048c2132f983326b3bb524c32d6d
unknown <gospeltruth18@gmail.com>
2019-10-07 13:20:59 +0100

    another change

--- university project/myenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\budescode\documents\coding\projects\university\myenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
